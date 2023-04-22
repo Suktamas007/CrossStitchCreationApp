5606c5ef2569f6fcc31c4772388c5f820c85c969
Suktamas <suktamas.p@hotmail.com>
2023-04-22 10:49:18 +0100

    Installed Emgu.CV packages to the project.

--- CrossStitchApp/Default.aspx.cs
+++ CrossStitchApp/Default.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Emgu.CV;
 
 namespace CrossStitchApp
 {
@@ -11,7 +12,7 @@ namespace CrossStitchApp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Mat img = new Mat();
         }
     }
 }
